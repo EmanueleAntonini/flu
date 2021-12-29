c2345ff740d737808fdb649207b0ace37e3fe26f
Emanuele Antonini <antonini.1753838@studenti.uniroma1.it>
2021-12-29 18:52:53 +0100

    Logging service added

---
+++ Logger/logProducer.js
@@ -0,0 +1,31 @@
+var amqp = require('amqplib/callback_api');
+
+function log(msg,cat){
+    amqp.connect('amqp://localhost', function(error0, connection) {
+    if (error0) {
+        throw error0;
+    }
+    connection.createChannel(function(error1, channel) {
+        if (error1) {
+            throw error1;
+        }
+        var exchange = 'flu_logs';
+        var args = process.argv.slice(2);
+        var message = msg + ' ' + cat;
+        var severity = (args.length > 0) ? args[0] : 'info';
+
+        channel.assertExchange(exchange, 'direct', {
+            durable: false
+        });
+        channel.publish(exchange, 'logs', Buffer.from(message));
+        console.log(" [x] Sent %s: '%s'", severity, message);
+    });
+
+    setTimeout(function() {
+        connection.close();
+        process.exit(0);
+    }, 500);
+});
+}
+
+module.exports = {log};

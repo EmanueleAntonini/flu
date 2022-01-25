c5aadc2f09ad21380a7c3fcc1bd529c05dc724b5
giorgiadarmi <darmiento.1711864@studenti.uniroma1.it>
2022-01-25 10:31:47 +0100

    piccole modifiche interfaccia Client

--- FluClient/flu-client/src/SearchForm.js
+++ FluClient/flu-client/src/SearchForm.js
@@ -7,7 +7,15 @@ class SearchForm extends React.Component {
             name: "",
             surname: "",
             twitterUsername: "",
-            influencer: ""
+            nome: "",
+            cognome: "",
+            nomeCompleto: "",
+            nomeTwitter: "",
+            twitterParams: "",
+            gNewsScore: "",
+            twitterScore: "",
+            fluScore: "",
+            influencer: "",
         };
         this.onChangeName = this.onChangeName.bind(this);
         this.onChangeSurname = this.onChangeSurname.bind(this);
@@ -33,6 +41,14 @@ class SearchForm extends React.Component {
         const response = await fetch("http://localhost:8080/influencer?name=" + this.state.name + "&surname=" + this.state.surname + "&twitter_username=" + this.state.twitterUsername);
         const data = await response.json();
         this.setState({ influencer: JSON.stringify(data) });
+        this.setState({ nome: JSON.stringify(data.name) });
+        this.setState({ cognome: JSON.stringify(data.surname) });
+        this.setState({ nomeCompleto: JSON.stringify(data.fullName) });
+        this.setState({ nomeTwitter: JSON.stringify(data.twitterUsername) });
+        this.setState({ twitterParams: JSON.stringify(data.twitterParams) });
+        this.setState({ gNewsScore: JSON.stringify(data.gNewsScore) });
+        this.setState({ twitterScore: JSON.stringify(data.twitterScore) });
+        this.setState({ fluScore: JSON.stringify(data.fluScore) });
         console.log(data);
         event.preventDefault()
     }
@@ -62,7 +78,15 @@ class SearchForm extends React.Component {
                     <br />
                     <input type="submit" value="Submit" />
                 </form>
-                <p>{this.state.influencer}</p>
+                <p><h2>Influencer Page</h2></p>
+                <p><label> Nome: </label>{this.state.nome}</p>
+                <p><label> Cognome: </label>{this.state.cognome}</p>
+                <p><label> Nome completo: </label>{this.state.nomeCompleto}</p>
+                <p><label> Twitter username: </label>{this.state.nomeTwitter}</p>
+                <p><label> Valori parametri twitter: </label>{this.state.twitterParams}</p>
+                <p><label> gNews Score: </label>{this.state.gNewsScore}</p>
+                <p><label> twitter Score: </label>{this.state.twitterScore}</p>
+                <p><label> flu Score: </label>{this.state.fluScore}</p>
             </div>
         );
     }

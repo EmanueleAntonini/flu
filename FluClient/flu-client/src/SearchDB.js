5fb939d1be64595091199e2ed00fd630b5ccab6c
Emanuele Antonini <antonini.1753838@studenti.uniroma1.it>
2022-01-28 13:03:39 +0100

    search from DB update

--- FluClient/flu-client/src/SearchDB.js
+++ FluClient/flu-client/src/SearchDB.js
@@ -5,26 +5,22 @@ class SearchDB extends React.Component {
         super(props);
         this.state = {
             numdoc: "",
-            documenti: "",
+            documenti: [],
         };
         this.handleSubmit = this.handleSubmit.bind(this);
     }
 
     async handleSubmit(event) {
         event.preventDefault()
-        console.log(this.state.name,this.state.surname,this.state.twitterUsername);
+        console.log(this.state.name, this.state.surname, this.state.twitterUsername);
         const response = await fetch("http://localhost:8080/getAllInfluencers");
         const data = await response.json();
         this.setState({ numdoc: JSON.stringify(data.total_rows) });
         var list = [];
-        for (var i=0; i < data.total_rows; ++i){
-            list.push("Documento:" + i);
-            list.push("Twitter id: ");
+        for (var i = 0; i < data.total_rows; ++i) {
             list.push(data.rows[i].id);
-            list.push("Identificativo documento: ");
-            list.push(data.rows[i].value);
         }
-        this.setState({ documenti: JSON.stringify(list) });
+        this.setState({ documenti: list });
         console.log(data);
         event.preventDefault()
     }
@@ -32,16 +28,24 @@ class SearchDB extends React.Component {
     render() {
         return (
             <div>
-                <form onSubmit={this.handleSubmit}>
-                    <br />
-                    <br />
-                    <br />
-                    <input type="submit" value="View flu_database" />
-                </form>
+
                 <p><h2>Documenti presenti in flu_database</h2></p>
                 <p><label><h5> Numero documenti presenti in flu_database : {this.state.numdoc}</h5></label></p>
                 <p><label><h5> Documenti: </h5></label></p>
-                <p>{this.state.documenti}</p>
+                <div>
+                    {this.state.documenti.map(id => {
+                        return (<div>
+                                    <p>{id}</p>
+                                    <br />
+                                </div>)
+                        }
+                    )}
+                </div>
+                <br />
+                <form onSubmit={this.handleSubmit}>
+
+                    <input type="submit" value="View flu_database" />
+                </form>
             </div>
         );
     }
